2ec1e7b1b268f8a70af25f983af388c608758f46
ugurosmanoqlu <ugurosmanoqlu@gmail.com>
2019-04-19 13:37:16 +0300

    Add project files.

---
+++ LoginWebApp/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginWebApp.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+    }
+}
